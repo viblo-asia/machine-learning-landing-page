39ac5c010767f9fec9607ad2af77cec438661de1
Nguyen Huu Kim <huukimit@gmail.com>
2018-09-07 09:12:50 +0700

    Update new service's description

--- contents/service-items-detail/contents.js
+++ contents/service-items-detail/contents.js
@@ -1,33 +1,16 @@
-export const langCodeDetect =
-  `<p>
-  Ứng dụng giải thuật <strong>trích xuất câu - sentences extraction</strong>
-  và <strong>trích xuất từ khóa - keywords extraction</strong>
-  trong <strong>xử lý ngôn ngữ tự nhiên - natural language processing</strong>
-  để tìm ra được các câu có nội dung quan trọng nhất trong bài viết Viblo.
-</p>
-
-<p>
-  Ứng dụng trong phần hiển thị nội dung tổng quát của các bài viết trên Viblo
-  và hỗ trợ sinh ra mô tả cho promotion trên các trang mạng xã hội như Facebook.
+export const langCodeDetect = `<p>
+  Hệ thống giúp xác định những phần chứa mã nguồn (code) trong bài viết được viết theo ngôn ngữ lập trình nào.
+  Từ đó dễ dàng hơn cho hệ thống để xử lý, highlight code trong bài viết...
 </p>`
 
-export const spamDetect =
-  `<p>
-  Sử dụng các kĩ thuật <strong>trích xuất từ khóa</strong> và rất nhiều kĩ thuật
-  xử lý ngôn ngữ tự nhiên khác vốn đã rất thành công trong việc ứng dụng
-  trong <strong>phân loại văn bản</strong> (được ghi nhận tại bài báo khoa học
-  của nhóm Machine Learning đăng tại hội nghị Tính toán tiến hóa và Trí tuệ nhân tạo
-  Châu Á Thái Bình Dương) và một số thay đổi để phù hợp cho tập dữ liệu của Viblo.
-</p>
-
-<p>
-  Ứng dụng trong việc hỗ trợ cho người dùng đánh tag của văn bản,
-  cũng như hỗ trợ việc chuẩn hóa các tag trong các bài viết của Viblo.
-  Từ đó có thể làm cải thiện hiệu năng SEO cho sản phẩm.
+export const spamDetect = `<p>
+  Hệ thống trí tuệ nhân tạo được training trên gần <strong>15,000 bài viết</strong>
+  về công nghệ hiện có trên Viblo cũng như các bài báo, quảng cáo khác
+  giúp xác định được đâu là bài đăng có nội dung không liên quan tới lĩnh vực công nghệ
+  với độ chính xác rất cao <strong>(99,7%)</strong> trong khoảng thời gian không để kể (ms).
 </p>`
 
-export const languageDetect =
-  `<p>
+export const languageDetect = `<p>
   Sử dụng <strong>giải thuật Đồ thị N-Gram (Graph-based N-Gram)</strong>
   và các kĩ thuật <strong>xử lý dữ liệu dạng kí tự Unicode</strong>
   để đưa ra giải thuật giúp định nghĩa ngôn ngữ xuất hiện trong văn bản
@@ -39,30 +22,16 @@ export const languageDetect =
   trợ giúp đắc lực cho việc tìm kiếm bằng Elasticsearch được chính xác hơn.
 </p>`
 
-export const autoTagging =
-  `<p>
-  Dựa trên <strong>giải thuật tính toán độ đo tương tự - words similarity</strong>
-  giữa các tags của Viblo để tìm ra được danh sách các tag có độ tương tự nhau về nội dung.
-</p>
-
-<p>
-  Ứng dụng trong việc hỗ trợ hỗ trợ cho người quản trị viên
-  tìm kiếm các thẻ tags trùng nhau dễ dàng hơn, cải thiện hiệu năng
-  của hệ thống Viblo và đóng góp một phần quan trọng trong khâu tiền xử lý
-  cho các dịch vụ khác trên Viblo như dịch vụ tự động gắn tag vào bài viết.
+export const autoTagging = `<p>
+  Mô hình được huấn luyện trên các bài viết có nội dung về công nghê, chứa nhiều
+  từ vựng và từ khóa về các chủ đề công nghệ hiện nay. Từ đó có thể đưa ra được
+  những tag hợp lý dựa trên nội dung bài viết trong thời gian không đáng kể.
 </p>`
 
-export const tagCompare =
-  `<p>
-  Sử dụng các kĩ thuật của xử lý ngôn ngữ tự nhiên như <strong>phân đoạn từ</strong>
-  và các <strong>thuật toán so sánh độ tương tự</strong> và tìm kiếm các
-  <strong>modification operations</strong> nhằm biến đổi văn bản thứ nhất thành văn bản thứ hai.
-</p>
-
-<p>
-  Ứng dụng trong việc hỗ trợ quản trị viên kiểm tra nhanh độ tương đồng
-  giữa hai văn bản đầu vào, giúp trích xuất ra các thành phần giống nhau
-  cũng như các thành phần đã bị biến đổi nhằm đánh giá chất lượng bài viết trên Viblo.
+export const tagCompare = `<p>
+  Training mô hình trên các tag có sẵn hiện có trên Viblo, đồng thời huấn luyện
+  với các bài viết chứa các tag này. Mô hình giúp lấy ra những tag gần nhất
+  (kể cả về mặt từ ngữ - words, lẫn về mặt ngữ nghĩa - meaning).
 </p>`
 
 export default [
--- contents/service-items-detail/index.js
+++ contents/service-items-detail/index.js
@@ -1,11 +1,11 @@
 import services from '../service-items'
-import descriptions from './descriptions'
+//import descriptions from './descriptions'
 import articles from './contents'
 import images from './images'
 
 const items = services.map((item, index) => {
   return Object.assign({}, item, {
-    description: descriptions[index],
+    //description: descriptions[index],
     image: images[index],
     contents: articles[index]
   })
