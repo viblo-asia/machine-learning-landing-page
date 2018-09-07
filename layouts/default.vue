0b85faf939088d17b3a9d1bad4c10165e83327a2
Nguyen Huu Kim <huukimit@gmail.com>
2018-09-07 17:18:27 +0700

    Update sticky header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
   <el-container class="viblo-app">
-    <el-header height="auto">
+    <el-header ref="stickyHeader" height="auto" class="el-header--sticky">
       <div class="viblo-container">
         <app-header/>
       </div>
@@ -26,18 +26,56 @@
     components: {
       AppHeader,
       AppFooter
+    },
+
+    mounted() {
+      this.dockedHeader()
+      window.addEventListener('scroll', this.dockedHeader)
+    },
+
+    beforeDestroy() {
+      window.removeEventListener('scroll', this.dockedHeader)
+    },
+
+    methods: {
+      dockedHeader() {
+        const stickyHeader = this.$refs.stickyHeader.$el
+        const isDocked = stickyHeader.classList.contains('el-header--docked')
+        const pageYOffset = window.pageYOffset
+
+        if (!isDocked && pageYOffset >= 20) {
+          stickyHeader.classList.add('el-header--docked')
+        } else if (isDocked && pageYOffset < 20) {
+          stickyHeader.classList.remove('el-header--docked')
+        }
+      }
     }
   }
 </script>
 
-<style lang="css">
-  .viblo-app {
-    min-height: 100vh;
-  }
+<style lang="scss">
+  @import '~assets/sass/variables/layout';
 
-  .viblo-container {
-    max-width: 1340px;
-    margin-left: auto;
-    margin-right: auto;
+  .viblo-app {
+    .el-header {
+      &--sticky {
+        background: #ffffff;
+        position: sticky;
+        top: 0;
+        -webkit-transition: all 0.3s ease-in-out;
+        -moz-transition: all 0.3s ease-in-out;
+        -ms-transition: all 0.3s ease-in-out;
+        -o-transition: all 0.3s ease-in-out;
+        transition: all 0.3s ease-in-out;
+        width: 100%;
+        z-index: 1000;
+      }
+
+      &--docked {
+        -webkit-box-shadow: 0 4px 4px 0 rgba(0, 0, 0, .1);
+        -moz-box-shadow: 0 4px 4px 0 rgba(0, 0, 0, .1);
+        box-shadow: 0 4px 4px 0 rgba(0, 0, 0, .1);
+      }
+    }
   }
 </style>
