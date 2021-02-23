3403442f69938054c81741021891a6dbbd4e5a11
@bui.huy.hoang <bui.huy.hoang@sun-asterisk.com>
2021-02-23 11:17:14 +0700

    feat use new api for auto tagging

--- app/api/index.js
+++ app/api/index.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 export const detectCode = data => axios.post('/_/programming_language_detect', data)
 export const detectSpam = data => axios.post('/_/viblo-lang-and-spam-detection/', data)
-export const tagCompare = data => axios.post('/_/tag_compare', data)
-export const autoTagging = data => axios.post('/_/auto_tagging', data)
+export const autoTagging = data => axios.post('/_/viblo-tag-generation/', data)
 export const detectLanguage = data => axios.post('/_/language_detect', data)
 export const detectPlagiarism = data => axios.post('/_/plagiarism_detect', data)
--- app/pages/_lang/auto-tagging.vue
+++ app/pages/_lang/auto-tagging.vue
@@ -3,19 +3,15 @@
     <el-form slot="form" :model="form" method="post" @submit.native.prevent="onSubmit">
       <el-form-item :label="$t('form.labels.enter_your_document')">
         <el-input
-          v-model="form.document"
+          v-model="form.content"
           :autosize="{ minRows: 6 }"
           type="textarea"
           @input="result = null"/>
       </el-form-item>
 
-      <el-form-item :label="$t('form.labels.enable_markdown_syntax')">
-        <el-switch v-model="form.is_markdown"/>
-      </el-form-item>
-
       <el-form-item class="flex flex--center">
         <el-button
-          :disabled="!form.document"
+          :disabled="!form.content"
           :loading="processing"
           type="primary"
           @click="onSubmit">
@@ -28,17 +24,12 @@
       <el-row>
         <el-col>
           <section-header :title="$t('form.labels.suggestions')" size="small"/>
-          <tags-list :tags="result ? result.tags_recommend : []">
-            <el-table-column
-              :label="$t('form.labels.total_posts')"
-              prop="number_posts"
-              width="150"
-              align="center"/>
-          </tags-list>
+          <tags-list :tags="result ? result : []"/>
           <btn-try-again @click="result = null"/>
         </el-col>
       </el-row>
     </div>
+
   </demo-service-page>
 </template>
 
@@ -67,7 +58,7 @@
       onSubmit () {
         this.processing = true
         return autoTagging(this.form)
-          .then(({ data }) => this.result = data.data)
+          .then(({ data }) => this.result = data.list_tag_predict)
           .catch(_ => this.$message.error(this.$t('errors.something_wrong')))
           .finally(() => this.processing = false)
       }
